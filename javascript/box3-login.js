9782a93f083636a0822859fd1b1e5ed08f9ca8f9
诸葛王鹏 <97856605+wzp12321@users.noreply.github.com>
2022-12-17 19:31:12 +0800

    Update box3-login.js

--- javascript/box3-login.js
+++ javascript/box3-login.js
@@ -1,6 +1,10 @@
 var islogin = document.querySelector("._3AspHqpBNnv2Z9vUyC6Fnx","._12b-ZtA2Hl4-wYcKqK83AR","._1SS6wc-FMtveQU1rUrkRW","._1CxJtBjdQtSJMiKtkt_Dvl", "._2eyZue_TPS-k5Q8e4IXi4H").innerText != '登录 / 注册' ? "登录" : "未登录";
 if (islogin == "登录"){
-    var touxiang = document.querySelector("body > div.desktop.website.app > header > div > div._2QvUYn1UWh6Jtz4XUSQ-3g > div.EtLtetKegDmJNWnqZpFw7 > div > div > picture > img").src;
-    var touxiang_hash = touxiang.substr("https://static.box3.codemao.cn/img/".length+1, "https://static.box3.codemao.cn/img/QmSHCR3aLropjVDjWrbK8Ydj9i5M8HNBkV3nevcYtFYTv8".length);
-    alert(touxiang_hash);
+    document.querySelector("._3AspHqpBNnv2Z9vUyC6Fnx","._12b-ZtA2Hl4-wYcKqK83AR","._1SS6wc-FMtveQU1rUrkRW","._1CxJtBjdQtSJMiKtkt_Dvl", "._2eyZue_TPS-k5Q8e4IXi4H").click();
+    var userdata = {
+        "name": document.querySelectorAll(".text")[0].textContent,//名称
+        "faith": Number(document.querySelectorAll(".text")[1].textContent),//信誉值
+        "concern": Number(document.querySelectorAll(".text")[2].textContent.slice(3)),//关注人数
+        "fans": Number(document.querySelectorAll(".text")[3].textContent.slice(3)),//粉丝人数
+    };
 };
