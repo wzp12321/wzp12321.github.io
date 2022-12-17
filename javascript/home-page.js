4c8dc665538fd354c87b3a0c50c7e14178a3447a
诸葛王鹏 <97856605+wzp12321@users.noreply.github.com>
2022-12-17 12:11:03 +0800

    Create home-page.js

---
+++ javascript/home-page.js
@@ -0,0 +1,20 @@
+VanillaTilt.init(document.querySelector(".your-element"), {
+		max: 25,
+		speed: 400,
+        glare:true,
+        "max-glare":1,
+	});
+	var mini = true;
+	function toggleSidebar() {
+ 	    if (mini) {
+    		console.log("opening sidebar");
+    		document.getElementById("mySidebar").style.width = "250px";
+    		document.getElementById("main").style.marginLeft = "250px";
+    		this.mini = false;
+  	    } else {
+    		console.log("closing sidebar");
+    		document.getElementById("mySidebar").style.width = "85px";
+    		document.getElementById("main").style.marginLeft = "85px";
+    		this.mini = true;
+  	    }
+	}
