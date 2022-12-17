f12ee3b0b4d9dfdd809a576d5fff786b7b15701e
诸葛王鹏 <97856605+wzp12321@users.noreply.github.com>
2022-12-17 13:47:26 +0800

    Update home-page.js

--- javascript/home-page.js
+++ javascript/home-page.js
@@ -4,15 +4,3 @@ VanillaTilt.init(document.querySelector(".your-element"), {
     glare:true,
     "max-glare":1,
 });
-var next = true;
-function toggleSidebar() {
-    if (next) {
-        document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-        next = false;
-    } else {
-        document.getElementById("mySidebar").style.width = "85px";
-        document.getElementById("main").style.marginLeft = "85px";
-        next = true;
-    };
-};
